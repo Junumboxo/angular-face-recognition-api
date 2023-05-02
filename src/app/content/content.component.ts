2018a05eba13a763601bdded60b3c26af13bf37b
Tatiana Barbova <44647094+Junumboxo@users.noreply.github.com>
2023-05-02 17:02:22 +0200

    Styling

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -37,7 +37,7 @@ export class ContentComponent {
     );
   }
 
-  onFileChanged(event) {;
+  onFileChanged(event) {
     var reader = new FileReader();
     var file:File = event.target.files[0];
 
